fcc455fe4e1d5042b9a5edf62b0e04b2a6c51dfb
Ander <user.name>
2019-04-12 01:16:43 +0200

    Preparado para screar todo catalunya y guardarlo en csv y objeto

--- scr/main.py
+++ scr/main.py
@@ -2,44 +2,65 @@
 """
 Created on Sun Apr  7 20:14:27 2019
 
-@author: Administrador
+@author: Ander Estebanez Centeno
 """
+#  Cambio de PYTHONPATH para poder importar las clases
 import sys
-sys.path.append('C:/Users/Administrador.WIN-K4PA08MDFCA/Documents/UOC/Ciclo de vida de los datos/Practica-1-Web-scraping')
+import pandas as pd
 
+path="C:/Users/Administrador.WIN-K4PA08MDFCA/Documents/UOC/Ciclo de vida de los datos/Practica-1-Web-scraping"
+sys.path.append(path)
 from scr.scraper import SubastaBOEScraper
+from scr.download_html import download_html
+import pickle
 
+#  Importar las listas de user-agent y proxys
+agents = pd.read_excel(path+"/input/user_agent.xlsx")
+agents = agents.to_dict(orient="records")
+
+proxys = pd.read_excel(path+"/input/proxys.xlsx")
+proxys = proxys.to_dict(orient="records")
+
+#  Se deine el dataset de salidaa
 output_file = "dataset.csv"
 
-sub = SubastaBOEScraper()
+#  Se iniciala  clase
+http = download_html(incognito=True, proxys=proxys, agents=agents,rSeed=120)
+sub = SubastaBOEScraper(http=http)
 
-sub.setFilter(provincia="08")
+#  Se crea el filtro de la provincia y luego se descarga el listado de subastas.
+#  A partir del primero se ejecuta con el anterior listSubasta de forma que se agreguen
+sub.setFilter(provincia="08") #  Barcelona
 sub.getSubastaLink()
-lsBcn=sub.listSubastas
 
-sub.setFilter(provincia="43")
-sub.getSubastaLink()
-lsTar=sub.listSubastas
+sub.setFilter(provincia="43") #  Tarragona
+sub.getSubastaLink(listSubastas=sub.listSubastas)
 
-sub.setFilter(provincia="17")
-sub.getSubastaLink()
-lsGir=sub.listSubastas
+sub.setFilter(provincia="17") #  Girona
+sub.getSubastaLink(listSubastas=sub.listSubastas)
 
-sub.setFilter(provincia="25")
-sub.getSubastaLink()
-lsLle=sub.listSubastas
+sub.setFilter(provincia="25") #Lleida
+sub.getSubastaLink(listSubastas=sub.listSubastas)
 
-lsTot=lsBcn+lsTar+lsGir+lsLle
-len(lsTot)
-sub.setListSubasta(lsTot)
+lsCat = sub.listSubastas
+len(sub.listSubastas)
+ 
+pickle.dump(sub.listSubastas,open(path+'/csv/20191104_listSubastas',"wb"))
+sub.setListSubasta(sub.listSubastas[0:200])
 sub.scrape()
+pickle.dump(sub.listSubastas,open(path+'/csv/20191104_listSubastas_scrape',"wb"))
 
-#Ejemplo 20 subastas Tarragona
-sub.setListSubasta(lsTar[0:20])
+data = pickle.load(open(path+'/csv/20191104_listSubastas',"rb"))
+
+
+save=sub.listSubastas
+len(save)
+sub.data2csv(path+"/csv")
+sub.setListSubasta(sub.listSubastas)
 sub.scrape()
+sub.listSubastas[18:19]
 
-import pickle
-filehandle = open('C:/Users/Administrador.WIN-K4PA08MDFCA/Documents/UOC/Ciclo de vida de los datos/Practica-1-Web-scraping/csv/output',"wb")
-pickle.dump(sub.listSubastas,filehandle)
-#filehandle = open('C:/Users/Administrador.WIN-K4PA08MDFCA/Documents/UOC/Ciclo de vida de los datos/Practica-1-Web-scraping/csv/output',"rb")
-#pickle.load(filehandle)
+a='Datos registrales: Finca 3024 de ALP, insctita en el registro propiedad Puigcerdà, Tomo 601, libro 39, folio 29. Finca urbana: entidad nº64 de edificio "Orbis Holidays" compuesto de dos cuerpos, letras A y B sito en Super Molina municipio de Alp: cuarto trasero en planta 2ª alta, cuerpo B; de unos 13 metros cuadrados aprox. Linda Norte, entidad nº65 de la misma planta;  Suy y Este vuelo de terreno común; y Oeste, escalera y pasillo de la planta en que està situado, cuota:  10 centèsimes por ciento (0.10%), respecto del total del inmueble que es finca nº2123, al folio 37 del libro 38º de Alp, tomo 600 del archivo. La finca no està coordinada gráficamente en el catastro'
+a[:a.find("\n")]
+a.strip("\n")
+a.replace(";",",")
